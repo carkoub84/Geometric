6cf7ff0417d8925c985b8968327f552ee41ae1be
carkoub84 <143051177+carkoub84@users.noreply.github.com>
2023-09-03 22:12:14 -0400

    Add files via upload

---
+++ Geometric.java
@@ -0,0 +1,87 @@
+class GeometricObject {
+    private String color;
+    private boolean filled;
+
+    public GeometricObject() {
+        color = "white";
+        filled = false;
+    }
+
+    public GeometricObject(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Color: " + color + "\nFilled: " + filled;
+    }
+}
+
+class Triangle extends GeometricObject {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle() {
+        side1 = 1.0;
+        side2 = 1.0;
+        side3 = 1.0;
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
+
+    public double getArea() {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+}
+
